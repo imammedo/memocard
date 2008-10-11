8b9bc978bff65f9639e1930ea9790bee6f703f02
Igor Mammedov <qwerty0987654321@mail.ru>
2008-10-12 00:10:08 +0400

    added gtk window for flashcard

--- flashcard.py
+++ flashcard.py
@@ -1,17 +1,43 @@
-import pynotify
+import gtk
+import gobject
 
+has_pynotify = False 
 
-if  pynotify.init("New mail trayicon"):
-	print "pynotify initialized"
-else:
-	print "there was a problem initializing the pynotify module"
+try:
+	import pynotify
+	if  pynotify.init("memocard"):
+		print "pynotify initialized"
+	else:
+		print "there was a problem initializing the pynotify module"
 
-class Card:
-	def __init__(self, word, definition):
-		self.word = word
-		self.definition = definition
-	def show(self):
+	has_pynotify = True
+
+	class CardGNotify(Card):
+		def __init__(self, word, definition):
+			self.word = word
+			self.definition = definition
+		def show(self):
+			n = pynotify.Notification(self.word, self.definition)
+			n.set_timeout(5000)
+			n.show()
+except:
+	print "no pynotify loaded"
+
+def show(word, definition):
+	if has_pynotify == True:
 		n = pynotify.Notification(self.word, self.definition)
 		n.set_timeout(5000)
 		n.show()
+	else:
+		msg = "%s\n%s" % (word, definition)
+		win = gtk.Window(type=gtk.WINDOW_POPUP)
+		#win = gtk.Window(type=gtk.WINDOW_TOPLEVEL)
+		win.add(gtk.Label(msg))
+		win.set_decorated(False)
+		win.set_gravity(gtk.gdk.GRAVITY_SOUTH_EAST)
+		win.move(10,10)
+		win.stick()
+		win.show_all()
+		gobject.timeout_add(10000, win.destroy)
+
 
