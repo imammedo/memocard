bb46970adfe5395e00428a283c3ad6953a026b0d
Igor Mammedov <niallain@gmail.com>
2008-10-13 17:41:07 +0400

    got rid of egg.trayicon

--- trayicon.py
+++ trayicon.py
@@ -1,6 +1,5 @@
 import gtk
 
-
 class TrayIcon:
 	def show(self):
 		return
@@ -12,25 +11,23 @@ class TrayIcon:
 		return
 
 class TrayIconGTK(TrayIcon):
+
 	def __init__(self, icon_file):
 		self.icon = gtk.StatusIcon()
 		self.icon.set_from_file(icon_file)
-		#self.img = gtk.Image()
-		#self.img.set_from_pixbuf(self.icon.get_pixbuf())
-		
+
 	def show(self):
 			self.icon.set_visible(True)
 
 	def hide(self):
-			self.icon.set_visible(False)
+		self.icon.set_visible(False)
+
 	def connect(self, event, handler):
 		return self.icon.connect(event, handler)
+
 	def connect(self, event, handler, obj):
 		return self.icon.connect(event, handler, obj)
 
-def getIcon():	
-	try:
-		import egg.trayicon
-	except:
-		return TrayIconGTK("gnome-dev-wavelan.png")
+def getIcon():
+	return TrayIconGTK("gnome-dev-wavelan.png")
 
